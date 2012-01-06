f38493cd6b9922aa937b28b8cddd8fe8eaacff73
Lucas Torri <lucastorri@gmail.com>
2012-01-06 14:07:04 -0200

    other changes to the start/stop script

--- bin/reindxr.sh
+++ bin/reindxr.sh
@@ -7,6 +7,7 @@
   # IRCLOG_LOGS_DIR
   # IRCLOG_PIDS_DIR
 
+
 SCRIPT=`basename "$0"`
 SCRIPT_DIR=`dirname "$0"`
 MAIN=co.torri.reindxr.Main
@@ -19,7 +20,7 @@ case $1 in
         ps -p `cat $PID_FILE`
         if [ $? -ne 0 ]
         then
-            exec 2>&1 $JAVA_HOME/bin/java -cp $CLASSPATH $MAIN $IRCLOG_DATA_DIR $IRCLOG_INDEX_DIR 1> $LOG_FILE &
+            exec 2>&1 $JAVA_HOME/bin/java -cp $CLASSPATH $MAIN "$IRCLOG_DATA_DIR" "$IRCLOG_INDEX_DIR" 1> $LOG_FILE &
             echo $! > $PID_FILE;
         fi
         ;;
