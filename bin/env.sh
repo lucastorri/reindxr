a235b25970132015aee6b47ca3dbae598c79231f
Lucas Torri <lucastorri@gmail.com>
2012-06-11 19:03:38 -0300

    making env flags optional

--- bin/env.sh
+++ bin/env.sh
@@ -2,8 +2,13 @@ REINDXR_HOME=$HOME/tmp/reindxr
 
 mkdir -p $REINDXR_HOME/{data,index,logs,pids}
 
-JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/
-DATA_DIR=$REINDXR_HOME/data
-INDEX_DIR=$REINDXR_HOME/index
-LOGS_DIR=$REINDXR_HOME/logs
-PIDS_DIR=$REINDXR_HOME/pids
+JAVA_HOME=${JAVA_HOME:=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/}
+DATA_DIR=${DATA_DIR:+${REINDXR_HOME}/data}
+INDEX_DIR=${DATA_DIR:+${REINDXR_HOME}/index}
+LOGS_DIR=${DATA_DIR:+${REINDXR_HOME}/logs}
+PIDS_DIR=${DATA_DIR:+${REINDXR_HOME}/pids}
+
+mkdir -p $DATA_DIR
+mkdir -p $INDEX_DIR
+mkdir -p $LOGS_DIR
+mkdir -p $PIDS_DIR
