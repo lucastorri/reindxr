6333259e7a726eee15f3d82ad6d6d857edebb9ef
Lucas Torri <lucastorri@gmail.com>
2012-06-11 18:09:10 -0300

    updating bin scripts

---
+++ bin/env.sh
@@ -0,0 +1,9 @@
+REINDXR_HOME=$HOME/tmp/reindxr
+
+mkdir -p $REINDXR_HOME/{data,index,logs,pids}
+
+JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/
+DATA_DIR=$REINDXR_HOME/data
+INDEX_DIR=$REINDXR_HOME/index
+LOGS_DIR=$REINDXR_HOME/logs
+PIDS_DIR=$REINDXR_HOME/pids
