af63244f51f498297de57c1ef6da7365bc58f551
Lucas Torri <lucastorri@gmail.com>
2013-07-26 14:21:20 +0200

    identation on the fragment builder

--- src/main/scala/co/torri/reindxr/index/TagFragmentBuilder.scala
+++ src/main/scala/co/torri/reindxr/index/TagFragmentBuilder.scala
@@ -18,14 +18,14 @@ case class TagFragmentBuilder(docFactory: DocFactory, snippetsOnly: Boolean, pre
 			return Array[String]()
 		}
 		
-        val termPositions = 
+    val termPositions =
 			for {
 				fragInfo <- fieldFragList.getFragInfos
 				subInfo <- fragInfo.getSubInfos
 				termOffset <- subInfo.getTermsOffsets
 			} yield (termOffset.getStartOffset, termOffset.getEndOffset, subInfo.getSeqnum)
 					
-		var fragments = termPositions.groupBy { case (termStart, termEnd, seqnum) =>
+		val fragments = termPositions.groupBy { case (termStart, termEnd, seqnum) =>
 			val startFragPosition = 
 				if (snippetsOnly) Some(source.lastIndexOf('\n', termStart)).map(index => if (index < 0) 0 else index + 1).get
 				else 0
@@ -33,7 +33,11 @@ case class TagFragmentBuilder(docFactory: DocFactory, snippetsOnly: Boolean, pre
 				if (snippetsOnly) Some(source.indexOf('\n', termEnd)).map(index => if (index < 0) source.size else index).get
 				else source.size
 			(startFragPosition, endFragPosition)
-		}.toList.sortBy(- _._2.size).take(maxFragmentsPerFile).map { case ((startFragPosition, endFragPosition), termsInFrag) =>
+		}
+    .toList
+    .sortBy(- _._2.size)
+    .take(maxFragmentsPerFile)
+    .map { case ((startFragPosition, endFragPosition), termsInFrag) =>
 			val terms = termsInFrag.sortBy(_._1)
 			val buf = new StringBuilder
 			
@@ -53,17 +57,19 @@ case class TagFragmentBuilder(docFactory: DocFactory, snippetsOnly: Boolean, pre
 		if (snippetsOnly && fragments.size < maxFragmentsPerFile) {
 			val b = fragments.toBuffer
 			source.linesWithSeparators.toList.takeWhile { l =>
-			  	if (!l.trim.isEmpty && !b.contains(l)) {
-			  		b += l
-			  	}
-			  	(b.size < maxFragmentsPerFile)
+        if (!l.trim.isEmpty && !b.contains(l)) {
+          b += l
+        }
+        (b.size < maxFragmentsPerFile)
 			}
 			b.toArray
 		} else if (!snippetsOnly && fragments.isEmpty) {
 			Array(source)
 		} else {
 			fragments
-		}.toArray
-    }
+		}
+    .toArray
+
+  }
 	
 }
